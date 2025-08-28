a65e40f74274055f9249a5e07d6272756e708f65
Nishant Kumar <this.is.nishantkumar1@gmail.com>
2025-08-28 14:23:04 +0530

    iudehiuedi

--- lib/view/screen/new_group_second.dart
+++ lib/view/screen/new_group_second.dart
@@ -18,6 +18,7 @@ class NewGroupSecond extends StatelessWidget {
 
   final TextEditingController groupNameTextEditingController =
       TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
