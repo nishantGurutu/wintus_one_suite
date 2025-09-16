78e5762100cfffc34310c5d252a8a47ee15e2617
Lalit <info@schotest.com>
2025-09-16 16:03:58 +0530

    new changes in appDeligate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,9 @@
 import Flutter
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
+import GoogleMaps
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +11,30 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Configure Firebase
+    FirebaseApp.configure()
+
+    // Configure Google Maps
+    GMSServices.provideAPIKey("AIzaSyDKZHjGSluLqRpdpgrqrghIlAIi2kZ9FAs")
+
+    // Configure local notifications
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+
+    // Register plugins
     GeneratedPluginRegistrant.register(with: self)
+
+    // Register for remote notifications
+    UNUserNotificationCenter.current().delegate = self
+    application.registerForRemoteNotifications()
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // Forward APNs token to Firebase
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 }
